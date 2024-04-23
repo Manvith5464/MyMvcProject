6ff225029a533ae01148a52985c195db495324eb
Manvith5464 <manvithkatkuri@usf.edu>
2024-04-23 16:06:05 -0400

    Editted

---
+++ Models/Place.cs
@@ -0,0 +1,30 @@
+
+public class Place
+
+{
+
+    public int BusinessId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Address { get; set; }
+
+    public string City { get; set; }
+
+    public string State { get; set; }
+
+    public string Zipcode { get; set; }
+
+    public string Phone { get; set; }
+
+    public string Website { get; set; }
+
+    public int TypeId { get; set; }
+
+    public string Image { get; set; }
+
+    public string Timings { get; set; }
+
+    public string Price { get; set; }
+
+}
---
+++ Models/Review.cs
@@ -0,0 +1,7 @@
+public class Review
+{
+    public int ReviewId { get; set; }
+    public int BusinessId { get; set; }
+    public int Rating { get; set; }
+    public string ReviewedBy { get; set; }
+}
